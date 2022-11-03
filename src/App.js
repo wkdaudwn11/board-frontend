b5896e2ba58df7b2ed35b1514bdba307df126c50
supertree-emjay <emjay@supertree.co>
2022-11-03 17:49:37 +0900

    feat: setting route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,27 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+import PrivateRouter from "./components/PrivateRouter";
+import Login from "./components/Login";
+import Join from "./components/Join";
+import BoardList from "./components/BoardList";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/join" element={<Join />} />
+        <Route
+          path="/"
+          element={
+            <PrivateRouter>
+              <BoardList />
+            </PrivateRouter>
+          }
+        />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
---
+++ src/components/PrivateRouter.jsx
@@ -0,0 +1,14 @@
+import { useLocation, Navigate } from "react-router-dom";
+
+const PrivateRouter = ({ children }) => {
+  const location = useLocation();
+  const accessToken = localStorage.getItem("ACCESS_TOKEN");
+
+  if (!accessToken) {
+    return <Navigate to="/login" state={{ from: location }} replace />;
+  }
+
+  return children;
+};
+
+export default PrivateRouter;
